247901e6a0776133407a165a0045bca3de4b3c64
Lee Weng Hong <whleexd@gmail.com>
2022-11-01 16:41:59 +0800

    Add files via upload

---
+++ L1Q2/src/l1q2/MyProfile.java
@@ -0,0 +1,12 @@
+package l1q2;
+
+public class MyProfile {
+    public static void main(String[] args) {
+        System.out.println("Full Name: Eggsy");
+        System.out.println("Matric Number:; 2200479/1");
+        System.out.println("Address: 4Q, Lorong Delima 11, Island Glades, Gelugor 11700 Pulau Pinang.");
+        System.out.println("Email: whleexd@gmail.com");
+        System.out.println("Contact: +60182614238");
+
+    }
+}
